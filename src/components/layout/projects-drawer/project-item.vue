4f500636befb4644fee55c05a2018a9ad1d345f7
Tobias Wälde <tobias.waelde@gmail.com>
2023-10-17 16:44:56 +0200

    fix context menu

--- src/components/layout/projects-drawer/project-item.vue
+++ src/components/layout/projects-drawer/project-item.vue
@@ -1,13 +1,33 @@
 <template>
-  <v-list-item :value="value" :title="_project.name" density="compact">
-    <template v-slot:prepend>
-      <IconCube :size="20" style="margin-right: 8px" />
+  <v-menu v-model="contextMenuOpen" location="right center">
+    <template v-slot:activator="{ props }">
+      <v-list-item
+        :value="value"
+        :title="_project.name"
+        density="compact"
+        v-bind="props"
+        @click.left="console.log('123')"
+        @click.left.prevent="contextMenuOpen = false"
+        @click.right.prevent="contextMenuOpen = true"
+      >
+        <template v-slot:prepend>
+          <IconCube :size="20" style="margin-right: 8px" />
+        </template>
+      </v-list-item>
     </template>
-  </v-list-item>
+
+    <v-list density="compact" style="padding: 0">
+      <v-list-item title="Delete" density="compact" @click="() => {}">
+        <template v-slot:prepend>
+          <IconTrash :size="20" style="margin-right: 8px" />
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
 
 <script lang="ts" setup>
-import { IconCube } from '@tabler/icons-vue';
+import { IconCube, IconTrash } from '@tabler/icons-vue';
 import { Project } from '../../../types/project';
 
 const props = defineProps<{
@@ -19,5 +39,6 @@ const http = useHttp();
 const res = await http.get<Project>(`/api/projects/${props.project.id}`);
 
 const _project = res.data;
-console.log(_project);
+
+const contextMenuOpen = ref<boolean>(false);
 </script>

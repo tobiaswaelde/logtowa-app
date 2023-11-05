2d49523daba93ccd99c14071f9f5e1c0d5d08c78
Tobias Wälde <tobias.waelde@gmail.com>
2023-11-05 22:14:39 +0100

    add stores

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_API_BASE_URL: string;
   readonly VITE_API_TOKEN: string;
+  readonly VITE_SOCKET_TOKEN: string;
   // more env variables...
 }
 
---
+++ src/store/logs-filter.ts
@@ -0,0 +1,59 @@
+import equal from 'deep-equal';
+import { defineStore } from 'pinia';
+import { ref, reactive, watch, onBeforeMount, onMounted } from 'vue';
+
+type FilterType = {
+  levels: string[];
+  scope: string;
+  message: string;
+};
+
+const getDefaultFilters = (): FilterType => ({
+  levels: ['error', 'warn', 'info'].toSorted(undefined),
+  scope: '',
+  message: '',
+});
+
+export const useLogsFilter = defineStore('logs-filter-store', () => {
+  const drawerOpen = ref<boolean>(false);
+  const isDefaultFilter = ref<boolean>(true);
+
+  const filter = reactive<FilterType>(getDefaultFilters());
+  const mounted = ref<boolean>(false);
+
+  watch([filter], () => {
+    if (!mounted.value) return;
+    localStorage.setItem('logs-filter', JSON.stringify(filter));
+    isDefaultFilter.value = equal(
+      { ...filter, levels: filter.levels.toSorted(undefined) },
+      getDefaultFilters(),
+    );
+  });
+
+  onBeforeMount(() => {
+    const value = localStorage.getItem('logs-filter');
+    if (value) {
+      const json = JSON.parse(value) as FilterType;
+      filter.levels = json.levels.toSorted(undefined);
+      filter.scope = json.scope;
+      filter.message = json.message;
+    }
+  });
+  onMounted(() => {
+    mounted.value = true;
+  });
+
+  const reset = () => {
+    const json = getDefaultFilters();
+    filter.levels = json.levels.toSorted(undefined);
+    filter.scope = json.scope;
+    filter.message = json.message;
+  };
+
+  return {
+    filter,
+    drawerOpen,
+    isDefaultFilter,
+    reset,
+  };
+});
